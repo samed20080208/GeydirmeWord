c97ea0a2244c259912ddd8d08c45c3686836a76a
samedyolchuyev@outlook.com <samedyolchuyev@outlook.com>
2024-04-22 22:12:28 +0400

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeydirmeWord
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void copyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(wordpad.Selection.Text))
+            {
+                Clipboard.SetText(wordpad.Selection.Text);
+            }            
+        }
+
+        private void cutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(wordpad.Selection.Text))
+            {
+                Clipboard.SetText(wordpad.Selection.Text);
+                wordpad.Selection.Text = "";
+            }
+        }
+
+        private void selectAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            wordpad.SelectAll();
+        }
+
+        private void pasteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string text = Clipboard.GetText();
+            if (string.IsNullOrEmpty(wordpad.Selection.Text))
+            {
+                wordpad.AppendText(text);
+            }
+            else
+            {
+                wordpad.Selection.Text = text;
+            }
+        }
+
+        private void okBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string filePath = FilePathBox.Text;
+            string text = File.ReadAllText(filePath);
+            if (string.IsNullOrEmpty(wordpad.Selection.Text))
+            {
+                wordpad.AppendText(text);
+            }
+            else
+            {
+                wordpad.Selection.Text = text;
+            }
+        }
+    }
+}
